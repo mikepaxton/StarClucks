1eb99b4b814088294615d59e31f07d5d6d1c2611
mike <paxton.mike@gmail.com>
2020-04-04 05:34:37 -0700

    Set main loop as a function

--- coopdoor.py
+++ coopdoor.py
@@ -1,24 +1,48 @@
 """
-This script opens and closes the chicken coop door at sunrise and dusk.  I use dusk to give the chickens
-ample time to get back in the coop at night.  Astral determines both of the those times based on current location.
-Schedule is used to actually schedule the opening and closing of the door.
-Runs on a Raspberry Pi with an L289 H-Bridge Controller
+coopdoor.py
 Author: Mike Paxton
-Modified on: 04/03/20 - Added buzzer when door opens/closes
+Creation Date: 03/15/20
+Python Version: 3
+
+Free and open for all to use.  But put credit where credit is due.
+
+OVERVIEW:-----------------------------------------------------------------------
+A simple program which opens and closes a chicken coop door at sunrise and dusk.
+I use dusk to give the chickens ample time to get back in the coop at night.
+Astral determines the times based on current location.
+Schedule is used to actually schedule the opening and closing of the door.
+See Wiki for more information:  https://github.com/mikepaxton/StarClucks/wiki
+
+PYTHON MODULES NEEDED:-----------------------------------------------------------
+gpiozero
+schedule
+astral
+
+HARDWARE REQUIREMENTS:-----------------------------------------------------------
+Runs on any Raspberry Pi.
+I am using a Raspberry Pi 2 because it uses less power than newer models which is important when running from a solar
+charged battery.
+A 16" Linear Actuator is used to raise and lower the coop door.
+An L289 H-Bridge is used to power and control the actuator.
+Momentary buttons for manually opening and closing the coop door.
+
+UPDATES:------------------------------------------------------------------------
+04/03/20 - Added buzzer when door opens/closes
+04/04/20 - Added main_loop as a function
 """
 # TODO: Consider adding some form of logging to record opening and closing date/time.
-from gpiozero import Button, Motor, Buzzer
+from gpiozero import Button, Motor
 import schedule
 import time
 import datetime
 from datetime import date
 import astral
+import sys
 
 #  GPIO pins used
 buttonOpen = Button(18)  # GPIO for open button.
 buttonClose = Button(23)  # GPIO for close button.
 motor = Motor(14, 15)  # First GPIO is open, second is close.
-bz = Buzzer(24)
 
 # Initiate variables for dawn_dusk function.
 sunrise = 0
@@ -35,17 +59,14 @@ def current_time():
 def open_door():
     motor.forward()
     print("Door opened at:", current_time())  # Comment out if you donn't wish to print
-    bz.on()  # Sound a buzzer to alert the chickens that door is about too open.
     time.sleep(3)
-    bz.off()
+
 
 
 def close_door():
     motor.backward()
     print("Door closed at:", current_time())  # Comment out if you donn't wish to print
-    bz.on()  # Sound a buzzer to alert the chickens that door is about too close.
     time.sleep(5)
-    bz.off()
 
 
 def dawn_dusk():
@@ -60,12 +81,8 @@ def dawn_dusk():
     schedule.every().day.at(dusk).do(close_door)
 
 
-dawn_dusk()  # Start the schedule
-
-
-try:
+def main_loop():
     while True:
-
         if buttonOpen.is_pressed:
             open_door()
         if buttonClose.is_pressed:
@@ -74,5 +91,14 @@ try:
         schedule.run_pending()
         time.sleep(1)
 
-except KeyboardInterrupt:
-    print('Control-C pressed')
+
+if __name__ == "__main__":
+    try:
+        dawn_dusk()  # Start the schedule
+        main_loop()
+    except RuntimeError as error:
+        print(error.args[0])
+    except KeyboardInterrupt:
+        print("\nExiting application\n")
+        # exit the application
+        sys.exit(0)
