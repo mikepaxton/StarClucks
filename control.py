913f7acebc496ff3a4da0d7db7a64cdd2d90c272
mikepaxton <R@@tMDP303b2>
2020-04-14 11:41:55 -0700

    LCD display is working in control.py

--- control.py
+++ control.py
@@ -9,7 +9,7 @@ Free and open for all to use.  But put credit where credit is due.
 OVERVIEW:-----------------------------------------------------------------------
 WARNING, this script is currently not functioning as intended.  It's actively being developed.
 This script controls and monitors various aspects of the chicken coop such temperature, lighting and solar output.
-Monitoring information is displayed on a 16x2 LCD.  A momentary push button is used to turn the display on and
+Monitoring information is displayed on a 20x4 LCD.  A momentary push button is used to turn the display on and
 start the cycling of the information.  The purpose for using the button is so that the LCD does not waste
 solar battery power.
 
@@ -17,14 +17,14 @@ PYTHON LIBRARIES NEEDED:--------------------------------------------------------
 gpiozero
 adafruit-circuitpython-am2320
 adafruit-circuitpython-ina260
-adafruit-circuitpython-charlcd
+i2c_lcd_driver
 Note: The remainder either will be installed as dependencies or already installed on the Raspberry Pi.
 
 HARDWARE REQUIREMENTS:-----------------------------------------------------------
 Runs on any Raspberry Pi.
 I am using a Raspberry Pi 2 because it uses less power than newer models which is important when running from a solar
 charged battery.
-1602 LCD for displaying information.
+2004 LCD for displaying information.
 Momentary button for activating LCD display.
 A relay board for the internal coop lighting.
 
@@ -32,18 +32,22 @@ UPDATES:------------------------------------------------------------------------
 This script controls and monitors various aspects of the chicken coop such as temperature, lighting and solar output.
 
 04/04/20 - Started working on LCD information display.
+0413/20 - LCD information is working correctly.
 """
 
 from gpiozero import Button, CPUTemperature
 import gpiozero
 import adafruit_am2320
 from adafruit_ina260 import INA260, Mode
-import adafruit_character_lcd.character_lcd_i2c as character_lcd
+import i2c_lcd_driver
 import board
 import busio
 import sys
 import time
 
+# Initialize lcd
+lcd = i2c_lcd_driver.lcd(0x3f)
+lcd.backlight(0)  # Turn LCD backlight off to save battery power.
 
 #  GPIO button used to toggle Light relay.
 lightsOnRelay = 17  # Coop light relay pin
@@ -56,40 +60,6 @@ lcdButton = Button(22)
 # Triggered by the output pin going low: active_high=False.
 # Initially off: initial_value=False
 coopLightRelay = gpiozero.OutputDevice(lightsOnRelay, active_high=False, initial_value=False)
-
-
-def lcddisplay():
-    # Initialise the lcd class
-    i2c = busio.I2C(board.SCL, board.SDA)
-    lcd = character_lcd.Character_LCD_I2C(i2c, 16, 2)
-    lcd.display = True
-    lcd.backlight = True  # Turn backlight on.
-    lcd.cursor = False  # Turn off the cursor.
-    cooptemp, coophumidity = am2320()  # Grab temp and humidity and display it.
-    lcd.message = "Temp: ", str(cooptemp) + "Humidity: ", str(coophumidity)  # print two line message.
-    time.sleep(3)
-    lcd.clear()
-    lcd.display = "CPU Temp: ", str(CPUTemperature)  # Display CPU temperature.
-    time.sleep(3)
-    lcd.clear()
-    current, voltage = solarstatus()  # Grab solar panel voltage, current, power and display it.
-    #lcd.message = "Solar Output" + "%.2f V %.2f mA" % (voltage, current)
-    lcd.cursor_position(0, 0)
-    lcd.message = "Solar: Volts %.2f" % voltage
-    lcd.cursor_position(0,1)
-    lcd.message = "Current %.2f mA" % current
-    time.sleep(3)
-    lcd.clear()
-    current, voltage = batterystatus()  # Grab battery voltage, current and display it.
-    #lcd.message = "Solar Panel Output" + "Voltage: %.2f V Current: %.2f mA" % (voltage, current)
-    lcd.cursor_position(0, 0)
-    lcd.message = "Batt: Volts %.2f" % voltage
-    lcd.cursor_position(0, 1)
-    lcd.message = "Current %.2f mA" % current
-    time.sleep(3)
-    lcd.clear()
-    lcd.display = False
-    lcd.backlight = False  # Turn backlight off to save battery power.
 
 
 def fahrenheit(temperature):
@@ -112,7 +82,7 @@ def solarstatus():
     ina260.mode = Mode.TRIGGERED  # Grab one current instance of sensor readings.
     current = ina260.current
     voltage = ina260.voltage
-    ina260.mode = Mode.SHUTDOWN  # Shut down sensor to save battery power.
+    #ina260.mode = Mode.SHUTDOWN  # SHUTDOWN mode returns an [Errno 121] Remote I/O error
     return current, voltage
 
 
@@ -122,7 +92,7 @@ def batterystatus():
     ina260.mode = Mode.TRIGGERED  # Grab one current instance of sensor readings.
     current = ina260.current
     voltage = ina260.voltage
-    ina260.mode = Mode.SHUTDOWN  # Shut down sensor to save battery power.
+    #ina260.mode = Mode.SHUTDOWN  # Shut down sensor to save battery power.
     return current, voltage
 
 
@@ -140,6 +110,35 @@ def toggle_coop_light_relay():
     coopLightRelay.toggle()
 
 
+def coopstats():
+    lcd.backlight(1)  # Turn LCD backlight on
+    lcd.lcd_clear()
+    cooptemp, coophudity = am2320()
+    lcd.lcd_display_string("Chicken Coop", 1, 4)  # String, row, column
+    lcd.lcd_display_string("Temp: " + str(cooptemp), 2, 0)
+    lcd.lcd_display_string("Humidity: " + str(coophudity), 3, 0)
+    time.sleep(5)
+    lcd.lcd_clear()
+    current, voltage = solarstatus()  # Grab solar panel voltage, current, power and display it.
+    lcd.lcd_display_string('Solar Status', 1, 4)
+    lcd.lcd_display_string("Voltage: %.2f" % voltage, 2, 0)
+    lcd.lcd_display_string("Current: %.2f" % current, 3, 0)
+    time.sleep(5)
+    lcd.lcd_clear()
+    # current, voltage = batterystatus()  # Grab battery voltage, current, power and display it.
+    # lcd.lcd_display_string('Battery Status', 1, 3)
+    # lcd.lcd_display_string("Voltage: %.2f" % voltage, 2, 0)
+    # lcd.lcd_display_string("Current: %.2f" % current, 3, 0)
+    # time.sleep(5)
+    # lcd.lcd_clear()
+    cpu = CPUTemperature()
+    lcd.lcd_display_string("CPU Temperature", 1, 2)
+    lcd.lcd_display_string('Temp: ' + str(cpu.temperature), 2, 0)  # Display CPU temperature.
+    time.sleep(5)
+    lcd.lcd_clear()
+    lcd.backlight(0)  # Turn LCD backlight off.
+
+
 def main_loop():
     # start by turning the relay off
     set_coop_light_relay(False)
@@ -147,7 +146,7 @@ def main_loop():
         if lightOnButton.is_pressed:
             toggle_coop_light_relay()
         if lcdButton.is_pressed:
-            lcddisplay()
+            coopstats()
         time.sleep(1)
 
 
