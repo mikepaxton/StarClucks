a6f659770b627d0cb00bf65c2574d5120d6c8fa9
mikepaxton <R@@tMDP303b2>
2020-04-26 15:26:29 -0700

    Cleaned up README file

--- control.py
+++ control.py
@@ -7,7 +7,6 @@ Python Version: 3
 Free and open for all to use.  But put credit where credit is due.
 
 OVERVIEW:-----------------------------------------------------------------------
-WARNING, this script is currently not functioning as intended.  It's actively being developed.
 This script controls and monitors various aspects of the chicken coop such temperature, lighting and solar output.
 Monitoring information is displayed on a 20x4 LCD.  A momentary push button is used to turn the display on and
 start the cycling of the information.  The purpose for using the button is so that the LCD does not waste
@@ -18,15 +17,16 @@ gpiozero
 adafruit-circuitpython-am2320
 adafruit-circuitpython-ina260
 i2c_lcd_driver
-Note: The remainder either will be installed as dependencies or already installed on the Raspberry Pi.
+Note: The remainder should be installed as dependencies or already installed on the Raspberry Pi.
 
 HARDWARE REQUIREMENTS:-----------------------------------------------------------
 Runs on any Raspberry Pi.
 I am using a Raspberry Pi 2 because it uses less power than newer models which is important when running from a solar
 charged battery.
 2004 LCD for displaying information.
-Momentary button for activating LCD display.
-A relay board for the internal coop lighting.
+Momentary button's for activating LCD and lighting.
+A relay board for the internal coop lighting.  I am using Pi-IOT Module with 5 built-in relays.
+Two ina260 sensors from Adafruit to monitor both solar and battery voltage/current.
 
 UPDATES:------------------------------------------------------------------------
 This script controls and monitors various aspects of the chicken coop such as temperature, lighting and solar output.
@@ -34,6 +34,11 @@ This script controls and monitors various aspects of the chicken coop such as te
 04/04/20 - Started working on LCD information display.
 04/13/20 - LCD information is working correctly.
 04/24/20 - Added batterystatus to LCD info dispayed.
+04/26/20 - Fixed issue with ina260's only using default i2c address.  Added address 0x40 to solar and 0x41 to battery.
+           Fixed issue with ina260's not displaying stats on LCD.  Changed Mode to CONTINUOUS as TRIGGERED was not working.
+           Fixed issue with Light Relay not working, was using wrong GPIO for Pi-IOT relay #1.  Now using GPIO 5
+           Changed Light On Button to GPIO 17
+           Changed LCD Button to GPIO 27
 """
 
 from gpiozero import Button, CPUTemperature
@@ -51,10 +56,10 @@ lcd = i2c_lcd_driver.lcd(0x3f)
 lcd.backlight(0)  # Turn LCD backlight off to save battery power.
 
 #  GPIO button used to toggle Light relay.
-lightsOnRelay = 17  # Coop light relay pin
-lightOnButton = Button(27)  # Coop light button.
+lightsOnRelay = 5  # Coop light relay pin
+lightOnButton = Button(17)  # Coop light button.
 # GPIO button to turn on/off LCD display.
-lcdButton = Button(22)
+lcdButton = Button(27)
 
 
 # create a relay object.
@@ -79,22 +84,24 @@ def am2320():
 
 def solarstatus():
     i2c = board.I2C()
-    ina260 = INA260(i2c)
-    ina260.mode = Mode.TRIGGERED  # Grab one current instance of sensor readings.
+    ina260 = INA260(i2c, 0x40)
+    ina260.mode = Mode.CONTINUOUS  # Grab one current instance of sensor readings.
     current = ina260.current
     voltage = ina260.voltage
+    power = ina260.power
     #ina260.mode = Mode.SHUTDOWN  # SHUTDOWN mode returns an [Errno 121] Remote I/O error
-    return current, voltage
+    return current, voltage, power
 
 
 def batterystatus():
     i2c = board.I2C()
-    ina260 = INA260(i2c)
-    ina260.mode = Mode.TRIGGERED  # Grab one current instance of sensor readings.
+    ina260 = INA260(i2c, 0x41)
+    ina260.mode = Mode.CONTINUOUS  # Grab one current instance of sensor readings.
     current = ina260.current
     voltage = ina260.voltage
+    power = ina260.power
     #ina260.mode = Mode.SHUTDOWN  # Shut down sensor to save battery power.
-    return current, voltage
+    return current, voltage, power
 
 
 def set_coop_light_relay(status):
@@ -120,16 +127,18 @@ def coopstats():
     lcd.lcd_display_string("Humidity: " + str(coophudity), 3, 0)
     time.sleep(5)
     lcd.lcd_clear()
-    current, voltage = solarstatus()  # Grab solar panel voltage, current, power and display it.
+    current, voltage, power = solarstatus()  # Grab solar panel voltage, current, power and display it.
     lcd.lcd_display_string('Solar Status', 1, 4)
     lcd.lcd_display_string("Voltage: %.2f" % voltage, 2, 0)
     lcd.lcd_display_string("Current: %.2f" % current, 3, 0)
+    lcd.lcd_display_string("Power: %.2f" % power, 4, 0)
     time.sleep(5)
     lcd.lcd_clear()
-    current, voltage = batterystatus()  # Grab battery voltage, current, power and display it.
+    current, voltage, power = batterystatus()  # Grab battery voltage, current, power and display it.
     lcd.lcd_display_string('Battery Status', 1, 3)
     lcd.lcd_display_string("Voltage: %.2f" % voltage, 2, 0)
     lcd.lcd_display_string("Current: %.2f" % current, 3, 0)
+    lcd.lcd_display_string("Power: %.2f" % power, 4, 0)
     time.sleep(5)
     lcd.lcd_clear()
     cpu = CPUTemperature()
